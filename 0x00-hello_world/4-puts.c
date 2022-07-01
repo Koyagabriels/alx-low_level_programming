5e8180dcbf190c8f871a46e475a5dd3438d94104
Koyagabriels <Koyagabriels@gmail.com>
2022-06-30 20:24:41 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle
+ *
+ * Return: Zero(0) for Success
+ */
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}

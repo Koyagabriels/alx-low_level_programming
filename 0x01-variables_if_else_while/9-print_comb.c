528c8b71d14b9d85193987d0169033d92b8a0e3d
Koyagabriels <Koyagabriels@gmail.com>
2022-07-02 15:31:49 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point for C code.
+ *
+ * Return: The Endpoint for C code.
+ */
+int main(void)
+{
+	int i = '0';
+
+	do {
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	} while (i <= '9');
+	putchar('\n');
+	return (0);
+}

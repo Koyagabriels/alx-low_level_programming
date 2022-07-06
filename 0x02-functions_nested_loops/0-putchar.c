b30145b934dc712e4953b9b7df8aa7d90c6eedf2
Koyagabriels <Koyagabriels@gmail.com>
2022-07-06 03:50:46 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,7 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putcaar('_putcar');
 	_putchar('\n');
 	return (0);
 }

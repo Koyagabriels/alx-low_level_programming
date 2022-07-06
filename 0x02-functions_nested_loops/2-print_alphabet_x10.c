07cd1cdf2eb73a295ce55ba830fb76ac052e7928
Koyagabriels <Koyagabriels@gmail.com>
2022-07-06 07:21:08 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int c = 'a';
+	
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
